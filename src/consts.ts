fedf3dea55d044f25524d38929334adf1cb32c15
diegoinjp <diego.vpda@gmail.com>
2023-12-09 19:47:54 +0900

    Fix Logo and description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Alan Gazzano'
-export const SITE_DESCRIPTION = 'Welcome to my portfolio!'
+export const SITE_DESCRIPTION = 'Japanese-Spanish Translator / Alan Gazzano'
