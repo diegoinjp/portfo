f6e3fffa50234939bf9b68bda2946a8aade29360
diegoinjp <diego.vpda@gmail.com>
2023-12-12 06:18:58 +0900

    Add ssr and netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// import node from '@astrojs/node';
+import node from '@astrojs/node';
 
 import preact from "@astrojs/preact";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
-  // adapter: node({
-  //   mode: 'standalone'
-  // }),
-  integrations: [tailwind(), preact()]
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [tailwind(), preact()],
+  output: "server",
+  adapter: netlify()
 });
